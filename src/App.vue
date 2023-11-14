e6ab3f19e5b6ebcf5fe2feed4b0c76287b95c4cd
123456hawkins <2398430768@qq.com>
2023-11-14 17:27:29 +0800

    新增插槽

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,31 @@
 <template>
-  <div style="display: flex;">
-    <div @click="switchCom(item, index)" class="tabs" :key="index" v-for="(item, index) in data"
-      :class="[active == index ? 'active' : '']">
-      <div>{{ item.name }}</div>
-    </div>
+  <div>
+    <Dialog>
+      <!-- 具名插槽 -->
+      <!-- #header是v-slot:header的简写 -->
+      <template #header>
+        <div>1</div>
+      </template>
+
+      <!-- 作用域插槽 -->
+      <template #default="{ person }">
+        <div v-if="person">{{ person.name }}--{{ person.age }}</div>
+      </template>
+
+
+      <!-- <template v-slot>
+        来自父组件的匿名插槽
+      </template> -->
+
+      <!-- 动态插槽 -->
+      <template #[name]>来自父组件的动态插槽</template>
+    </Dialog>
   </div>
-  <component :is="comId" style="border: 2px solid black;"></component>
 </template>
 
 <script setup lang='ts'>
-import { reactive, ref, markRaw, shallowRef } from 'vue'
-import A from './example/A.vue';
-import B from './example/B.vue';
-import C from './example/C.vue'
-
-const comId = shallowRef(A)
-let active = ref(0)
-const data = reactive([
-  {
-    name: 'A组件',
-    com: markRaw(A) //markRaw跳过数据劫持
-  },
-  {
-    name: 'B组件',
-    com: markRaw(B)
-  },
-  {
-    name: 'C组件',
-    com: markRaw(C)
-  },
-])
-const switchCom = (item, index) => {
-  comId.value = item.com
-  active.value = index
-}
+import Dialog from './components/Dialog/index.vue'
+import { ref } from 'vue'
+let name = ref('footer')
 </script>
-<style scoped lang='scss'>
-.active {
-  background-color: red;
-}
-
-.tabs {
-  border: 1px solid black;
-  margin: 10px;
-  height: 50px;
-  width: 80px;
-  padding: 5px 10px;
-  cursor: pointer;
-
-
-}
-</style>
+<style scoped lang='scss'></style>
---
+++ src/components/Dialog/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <header class="header">
+      <!-- 作用域插槽 -->
+      <div>
+        <slot name="header"></slot>
+        <div v-for="(item, index) in peopleList " :key="index">
+          <!-- 传值给父组件 -->
+          <slot :person="item"></slot>
+        </div>
+      </div>
+    </header>
+    <main class="main">
+      <!-- 匿名插槽 -->
+      <slot></slot>
+    </main>
+    <footer class="footer">
+      <!-- 动态插槽 -->
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { reactive } from 'vue'
+type Peo = {
+  name: string
+  age: number
+}
+let peopleList = reactive<Peo[]>([
+  {
+    name: 'hawkins',
+    age: 22
+  },
+  {
+    name: 'hawkins3',
+    age: 2224
+  },
+  {
+    name: 'hawkins4',
+    age: 2228
+  },
+  {
+    name: 'hawkins5',
+    age: 224
+  },
+])
+</script>
+<style scoped lang='scss'></style>
