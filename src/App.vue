4eb6e95e47f7a11516011e2d7abc1be33dc648ed
123456hawkins <2398430768@qq.com>
2023-11-15 14:52:32 +0800

    新增transition组件

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
 <template>
-  <el-button type="primary" @click="flag = !flag">切换组件</el-button>
-  <!-- 只缓存A组件的 -->
-  <!-- keeplive只能有一个子节点 -->
-  <KeepAlive>
-    <A v-if="flag"></A>
-    <B v-else></B>
-  </KeepAlive>
+  <div>
+    <content></content>
+  </div>
 </template>
 
 <script setup lang='ts'>
-import { ref, onActivated, onDeactivated, onMounted, onUnmounted } from 'vue'
-import A from './example/A.vue';
-import B from './example/B.vue';
-let flag = ref<boolean>(true)
-
-// 这两个生命周期只有开启keepalive才能使用
-onMounted(() => {
-  console.log('初始化');
-
-})
-onUnmounted(() => {
-  console.log('卸载');
-
-})
-onActivated(() => {
-  console.log('keepalive初始化');
-
-})
-onDeactivated(() => {
-  console.log('keepalive卸载');
-
-})
+import { ref, reactive } from 'vue'
+import content from '@/Content/index.vue'
 </script>
 <style scoped lang='scss'></style>
--- src/Content/index.vue
+++ src/Content/index.vue
@@ -1,8 +1,104 @@
 <template>
-  <div>content</div>
+  <div class="content">
+    <button @click="flag = !flag">switch</button>
+    <!-- 要加animate__animated前缀 -->
+    <transition @before-enter="onBeforeEnter" @enter="onEnter" @after-enter="onAfterEnter"
+      @enter-cancelled="onEnterCancelled" @before-leave="onBeforeLeave" @leave="onLeave" @after-leave="onAfterLeave"
+      @leave-cancelled="onLeaveCancelled" :duration="3000" name="fade">
+      <div v-if="flag" class="box"></div>
+    </transition>
+  </div>
 </template>
 
 <script setup lang='ts'>
-import { ref, reactive } from 'vue'
+import { ref } from 'vue'
+// 导入animate库
+import 'animate.css'
+import gsap from 'gsap'
+let flag = ref<boolean>(true)
+
+const onBeforeEnter = (el: Element) => {
+  console.log('插入之前', el);
+  gsap.set(el, {
+    width: 0,
+    height: 0
+  })
+
+}
+const onEnter = (el: Element, done: gsap.Callback) => {
+  console.log('插入到dom的下一帧', el);
+  gsap.to(el, {
+    width: 200,
+    height: 200,
+    onComplete: done
+  })
+}
+const onAfterEnter = (el: Element) => {
+  console.log('插入到dom完成后', el);
+}
+const onEnterCancelled = (el: Element) => {
+  console.log('插入到dom取消', el);
+}
+const onBeforeLeave = (el: Element) => {
+  console.log('离开dom之前', el);
+}
+const onLeave = (el: Element, done: gsap.Callback) => {
+  console.log('离开dom的下一帧', el);
+  gsap.to(el, {
+    width: 0,
+    height: 0,
+    onComplete: done
+  })
+}
+const onAfterLeave = (el: Element) => {
+  console.log('离开dom完成后', el);
+}
+const onLeaveCancelled = (el: Element) => {
+  console.log('离开dom取消', el);
+
+}
 </script>
-<style scoped lang='scss'></style>
+
+<style scoped lang='scss'>
+// 动画要放在元素属性下面,不然读不到
+.box {
+  height: 200px;
+  width: 200px;
+  background-color: red;
+}
+
+// .fade-enter-active {
+//   transition: all 1.5s ease;
+// }
+
+.fade-enter-from {
+  height: 0;
+  width: 0;
+}
+
+.fade-enter-to {
+  height: 100px;
+  width: 100px;
+}
+
+// .fade-leave-active {
+//   transition: all 1.5s ease;
+// }
+
+.fade-leave-from {
+  height: 100px;
+  width: 100px;
+}
+
+.fade-leave-to {
+  height: 0;
+  width: 0;
+}
+
+
+.content {
+  height: 600px;
+  width: 600px;
+  background-color: green;
+}
+</style>
