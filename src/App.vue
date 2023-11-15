91f0c7d0fe6574cd9145b9b7d86b5e6f46b91b57
123456hawkins <2398430768@qq.com>
2023-11-15 11:08:41 +0800

    新增keepalive

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <div class="pageConatiner">我是父级
-    <Teleport to="body">
-      <teleP></teleP>
-    </Teleport>
-    <div class="footer" id="footer"></div>
-  </div>
+  <el-button type="primary" @click="flag = !flag">切换组件</el-button>
+  <!-- 只缓存A组件的 -->
+  <!-- keeplive只能有一个子节点 -->
+  <KeepAlive>
+    <A v-if="flag"></A>
+    <B v-else></B>
+  </KeepAlive>
 </template>
 
 <script setup lang='ts'>
-import { ref, reactive } from 'vue'
-import teleP from './components/teleP.vue';
+import { ref, onActivated, onDeactivated, onMounted, onUnmounted } from 'vue'
+import A from './example/A.vue';
+import B from './example/B.vue';
+let flag = ref<boolean>(true)
+
+// 这两个生命周期只有开启keepalive才能使用
+onMounted(() => {
+  console.log('初始化');
+
+})
+onUnmounted(() => {
+  console.log('卸载');
+
+})
+onActivated(() => {
+  console.log('keepalive初始化');
+
+})
+onDeactivated(() => {
+  console.log('keepalive卸载');
+
+})
 </script>
-<style scoped lang='scss'>
-* {
-  padding: 0;
-  margin: 0;
-}
-
-.pageConatiner {
-  height: 50vh;
-  width: 80vh;
-  background-color: greenyellow;
-
-  .footer {
-    height: 50%;
-    border: 1px solid black;
-  }
-}
-</style>
+<style scoped lang='scss'></style>
