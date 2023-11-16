04f34b949a4065d9525039a652742c02c0837b0a
123456hawkins <2398430768@qq.com>
2023-11-16 17:15:10 +0800

    新增hooks

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
 <template>
-  <div style="height: 800;overflow: auto;">
-    <div v-for="(item, index) in arr" :key="index">
-      <img height="500" :data-index="item" v-lazy="item" width="360" alt="">
-    </div>
-  </div>
+  <div><img id="img" src="./assets/images/c1.jpg"></div>
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
-import type { Directive } from 'vue'
-const images: Record<string, { default: string }> = import.meta.globEager('./assets/images/*.*')
-let arr = Object.values(images).map(v => v.default)
+import useBase64 from './hooks'
+useBase64({ el: '#img' }).then(res => {
+  console.log(res.Baseurl);
 
-let vLazy: Directive<HTMLImageElement, string> = async (el, binding) => {
-  let url = await import('./assets/logo.svg')
-  el.src = url.default;
-  let observer = new IntersectionObserver((entries) => {
-    console.log(entries[0], el)
-    if (entries[0].intersectionRatio > 0 && entries[0].isIntersecting) {
-      setTimeout(() => {
-        el.src = binding.value;
-        observer.unobserve(el)
-      }, 2000)
-    }
-  })
-  observer.observe(el)
-}
-
+})
 </script>
-
-<style scoped lang='less'></style>
+<style scoped lang='scss'></style>
