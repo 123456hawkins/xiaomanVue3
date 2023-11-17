fe86dfd3890757bf8b8d9dbe6045a278f1320875
123456hawkins <2398430768@qq.com>
2023-11-17 10:37:24 +0800

    新增样式穿透选择器

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <el-input placeholder="测试代码"></el-input>
+    <el-input placeholder="测试代码" class="input"></el-input>
   </div>
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
 </script>
-<style scoped lang='scss'></style>
+<style scoped lang='scss'>
+.input {
+  width: 600px;
+// vue3使用:deep(<类名>)的方式
+  :deep(.el-input__inner) {
+    background-color: green;
+  }
+}
+</style>
