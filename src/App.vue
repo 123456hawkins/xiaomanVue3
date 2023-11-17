fa81e29dffb92f3de126eb9cba579ba8bc74df94
123456hawkins <2398430768@qq.com>
2023-11-17 16:17:02 +0800

    新增环境变量

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <template>
-  <div ref="xiaoman">
-    {{ text }}
-  </div>
-  <button @click="change">change div</button>
+  <div></div>
 </template>
-  
+
 <script setup lang='ts'>
-import { ref, nextTick } from 'vue';
+import { ref, reactive, onMounted } from 'vue'
+onMounted(() => {
+  console.log(import.meta.env);
 
-const text = ref('小满开飞机')
-const xiaoman = ref<HTMLElement>()
-
-const change = async () => {
-  text.value = '小满不开飞机'
-  console.log(xiaoman.value?.innerText) //小满开飞机
-  await nextTick();
-  console.log(xiaoman.value?.innerText) //小满不开飞机
-}
-
-
+})
 </script>
-
-
-<style  scoped></style>
+<style scoped lang='scss'></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+// loadEnv读取环境变量
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import tsx from './plugin/index'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(), tsx()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: "@import './src/bem.scss';"
+export default ({ mode }: any) => {
+  console.log(mode);
+ 
+  console.log(loadEnv(mode, process.cwd()));
+
+  return defineConfig({
+    plugins: [
+      vue(), tsx()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: "@import './src/bem.scss';"
+        }
       }
     }
-  }
 
-})
+  })
+}
